cdb63f04dcab597b315604ce001ecea1a816f887
stefoto85 <you@example.com>
2026-02-24 01:15:47 +0200

    fix error

--- common/sitemaps.py
+++ common/sitemaps.py
@@ -1,12 +1,18 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
+
 class StaticViewSitemap(Sitemap):
     priority = 0.8
     changefreq = "weekly"
 
     def items(self):
-        return ["past-events"]
+        return [
+            "home",
+            "about",
+            "contacts",      # ✅ беше "contact"
+            "past-events",
+        ]
 
     def location(self, item):
         return reverse(item)
