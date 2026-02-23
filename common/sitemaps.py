9dd51f94179bb6315997a844784a4f7099d661f4
stefoto85 <you@example.com>
2026-02-24 01:11:05 +0200

    test stamp error

--- common/sitemaps.py
+++ common/sitemaps.py
@@ -1,17 +1,12 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
 
-
 class StaticViewSitemap(Sitemap):
     priority = 0.8
     changefreq = "weekly"
 
     def items(self):
-        return [
-            'home',
-            'contact',
-            'past-events',
-        ]
+        return ["past-events"]
 
     def location(self, item):
         return reverse(item)
