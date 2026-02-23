531bc9763d416ec3f0406ed2680c936940f8f64a
stefoto85 <you@example.com>
2026-02-24 01:33:59 +0200

    remove phone nuber and update sitemap

--- common/sitemaps.py
+++ common/sitemaps.py
@@ -1,18 +1,17 @@
 from django.contrib.sitemaps import Sitemap
 from django.urls import reverse
-
+from django.utils.translation import override
 
 class StaticViewSitemap(Sitemap):
     priority = 0.8
     changefreq = "weekly"
 
     def items(self):
-        return [
-            "home",
-            "about",
-            "contacts",      # ✅ беше "contact"
-            "past-events",
-        ]
+        return ["home", "about", "contacts", "past-events"]
 
-    def location(self, item):
-        return reverse(item)
+    def get_urls(self, page=1, site=None, protocol=None):
+        urls = []
+        for lang in ("bg", "en"):
+            with override(lang):
+                urls.extend(super().get_urls(page=page, site=site, protocol=protocol))
+        return urls
