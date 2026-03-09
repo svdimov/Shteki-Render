431c0f94218a6ce30e7cbcd03ba9925b6560ff89
stefoto85 <you@example.com>
2026-03-10 01:56:18 +0200

    fix reset 3

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,42 +1,53 @@
-
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
-from django.urls import path, include
+from django.urls import include, path
+
 from accounts import views
-from accounts.forms import CustomSetPasswordForm
-from accounts.views import CustomLoginView, CustomPasswordResetConfirmView, ActivationEmailSentView, ActivateAccountView
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name='register'),
 
-    path("activation-email-sent/",ActivationEmailSentView.as_view(),name="activation-email-sent",),
-    path("activate/<uidb64>/<token>/",ActivateAccountView.as_view(),name="activate-account",),
+    path(
+        "activation-email-sent/",
+        views.ActivationEmailSentView.as_view(),
+        name="activation-email-sent",
+    ),
+    path(
+        "activate/<uidb64>/<token>/",
+        views.ActivateAccountView.as_view(),
+        name="activate-account",
+    ),
 
-    path("login/", CustomLoginView.as_view(), name='login'),
+    path("login/", views.CustomLoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name='logout'),
 
-    # Forgot password
-    path("password-reset/", auth_views.PasswordResetView.as_view(
-        template_name='profiles/password_reset.html'
-    ), name='password_reset'),
+    path(
+        "password-reset/",
+        views.CustomPasswordResetView.as_view(),
+        name='password_reset',
+    ),
+    path(
+        "password-reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='profiles/password_reset_done.html'
+        ),
+        name='password_reset_done',
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        views.CustomPasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='profiles/password_reset_complete.html'
+        ),
+        name='password_reset_complete',
+    ),
 
-    path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(
-        template_name='profiles/password_reset_done.html'
-    ), name='password_reset_done'),
-
-    path("reset/<uidb64>/<token>/", CustomPasswordResetConfirmView.as_view(
-        form_class=CustomSetPasswordForm,
-        template_name='profiles/password_reset_confirm.html'
-    ), name='password_reset_confirm'),
-
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
-        template_name='profiles/password_reset_complete.html'
-    ), name='password_reset_complete'),
-
-    # Change password
     path('password-change/', views.ChangePasswordView.as_view(), name='password-change'),
     path('password-change/done/', views.ChangePasswordDoneView.as_view(), name='password-change-done'),
-
 
     path("profile/<int:pk>/", include([
         path('', views.ProfileDetailView.as_view(), name='profile-details'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,30 @@
-from django.contrib.auth import views as auth_views
-from django.contrib.auth import get_user_model, login
-from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.contrib.auth.views import  LoginView, PasswordResetConfirmView
+import logging
 
-from django.shortcuts import get_object_or_404
-
-from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, UpdateView, DetailView, DeleteView
-
-from accounts.forms import AppUserCreationForm, ProfileEditForm, CustomAuthenticationForm, CustomChangePasswordForm
-from accounts.models import Profile
-
+from django.contrib.auth import get_user_model, views as auth_views
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.views import LoginView, PasswordResetConfirmView
+from django.db import transaction
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse, reverse_lazy
 from django.utils import timezone
-from django.contrib.auth.forms import PasswordResetForm
-from django.shortcuts import render
-
-from accounts.utils import send_activation_email, send_password_reset_email
-from django.shortcuts import redirect
-
-from django.views.generic import TemplateView
-
-
-from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.core.mail import send_mail
-from django.shortcuts import render
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
 from django.views import View
+from django.views.generic import CreateView, DeleteView, DetailView, TemplateView, UpdateView
 
+from accounts.forms import (
+    AppUserCreationForm,
+    CustomAuthenticationForm,
+    CustomChangePasswordForm,
+    CustomSetPasswordForm,
+    ProfileEditForm,
+)
+from accounts.models import Profile
 from accounts.tokens import account_activation_token
+from accounts.utils import send_activation_email, send_password_reset_email
 
-import logging
-from django.db import transaction
 logger = logging.getLogger(__name__)
-
 UserModel = get_user_model()
 
 
@@ -55,18 +45,24 @@ class RegisterView(CreateView):
 
             return redirect(self.get_success_url())
 
-        except Exception as e:
-            logger.exception("Registration activation email failed for %s", form.cleaned_data.get("email"))
-            form.add_error(None, "Не успяхме да изпратим email за активация. Моля, опитай отново.")
+        except Exception:
+            logger.exception(
+                "Registration activation email failed for %s",
+                form.cleaned_data.get("email"),
+            )
+            form.add_error(
+                None,
+                "Не успяхме да изпратим email за активация. Моля, опитай отново."
+            )
             return self.form_invalid(form)
+
 
 class CustomLoginView(LoginView):
     authentication_form = CustomAuthenticationForm
     template_name = 'profiles/login.html'
 
     max_attempts = 3
-    ock_minutes = 15
-
+    lock_minutes = 15
 
     def dispatch(self, request, *args, **kwargs):
         if request.method == 'POST':
@@ -77,22 +73,27 @@ class CustomLoginView(LoginView):
                     return render(request, 'profiles/account-login.html')
             except UserModel.DoesNotExist:
                 pass
+
         return super().dispatch(request, *args, **kwargs)
 
     def form_valid(self, form):
         user = form.get_user()
         user.failed_login_attempts = 0
-        user.save()
+        user.is_locked = False
+        user.save(update_fields=['failed_login_attempts', 'is_locked'])
         return super().form_valid(form)
 
     def form_invalid(self, form):
         email = form.data.get('username')
+
         try:
             user = UserModel.objects.get(email=email)
+
             if user.is_locked:
                 return render(self.request, 'profiles/account-login.html')
 
             user.failed_login_attempts += 1
+
             if user.failed_login_attempts >= self.max_attempts:
                 user.is_locked = True
                 user.failed_login_attempts = 0
@@ -116,13 +117,31 @@ class CustomLoginView(LoginView):
         return super().form_invalid(form)
 
 
+class CustomPasswordResetView(auth_views.PasswordResetView):
+    template_name = 'profiles/password_reset.html'
+    success_url = reverse_lazy('password_reset_done')
+
+    def form_valid(self, form):
+        email = form.cleaned_data["email"]
+
+        for user in form.get_users(email):
+            try:
+                send_password_reset_email(self.request, user)
+            except Exception:
+                logger.exception(
+                    "Failed to send manual password reset email to %s",
+                    user.email,
+                )
+
+        return HttpResponseRedirect(self.get_success_url())
+
+
 class ProfileDetailView(LoginRequiredMixin, DetailView):
     model = Profile
     template_name = 'profiles/profile-details.html'
     context_object_name = 'profile'
 
     def get_object(self, queryset=None):
-        # We assume the Profile model has a field named 'user' that links to the UserModel.
         return get_object_or_404(Profile, user__pk=self.kwargs.get('pk'))
 
 
@@ -147,8 +166,6 @@ class ProfileDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     success_url = reverse_lazy('home')
 
     def test_func(self):
-        # This ensures a user can only delete their own account.
-        # self.get_object() safely retrieves the user instance from the URL's pk.
         return self.request.user == self.get_object()
 
 
@@ -163,27 +180,20 @@ class ChangePasswordDoneView(auth_views.PasswordChangeDoneView):
 
 
 class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    form_class = CustomSetPasswordForm
+    template_name = 'profiles/password_reset_confirm.html'
+
     def form_valid(self, form):
         response = super().form_valid(form)
         user = self.user
         user.is_locked = False
         user.failed_login_attempts = 0
-        user.save()
+        user.save(update_fields=['is_locked', 'failed_login_attempts'])
         return response
-
-#
-
 
 
 class ActivationEmailSentView(TemplateView):
     template_name = 'profiles/activation-email-sent.html'
-
-
-
-
-
-
-
 
 
 class ActivateAccountView(View):
